cb02cd6c58765c0fc37ce4d2592c49c57470afee
Mirosh757 <dartyr2016@gmail.com>
2025-05-01 22:45:39 +0800

    Переделана таблица doctor. Атрибут name разбит на три атрибута name, family, patronymic. Переделано заполнение базы данных

--- 2_Lab_MongoDb/models/doctor.js
+++ 2_Lab_MongoDb/models/doctor.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const doctorSchema = new mongoose.Schema({
     name: String,
+    family: String,
+    patronymic: String,
     address: String,
     passport_details: String,
     date_birth: Date
